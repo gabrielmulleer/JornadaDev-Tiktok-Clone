bae9101baa0791f1bb2f493128148d6d82320e7a
Gabriel Müller <75191241+gabrielmulleer@users.noreply.github.com>
2023-04-02 16:39:27 -0300

    feature: add aula05 firebase integration (#3)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Video from "./pages/Video";
+import db from "./config/firebase";
+import { collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+  const [video, setVideos] = useState([]);
+
+  async function getVideos() {
+    const videosCollection = collection(db, "videos");
+    const videosSnapshot = await getDocs(videosCollection);
+    const videosList = videosSnapshot.docs.map((doc) => doc.data());
+    setVideos(videosList);
+  }
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
     <div className="App">
       <div className="app__videos">
-        <Video
-          likes={111}
-          messages={222}
-          shares={333}
-          name="Gabriel"
-          description="Brecker o goleiro"
-          music="Música épica"
-          url="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/brecker2.mp4?alt=media&token=b7e3ebf7-89a0-47a0-8a34-66f1176a71e3"
-        />
-        <Video
-          likes={111}
-          messages={222}
-          shares={333}
-          name="Pedro"
-          description="Bird olhando para a camera"
-          music="Clap your hands"
-          url="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
-        />
+        {video.map((item) => {
+          return (
+            <Video
+              likes={item.likes}
+              messages={item.messages}
+              shares={item.shares}
+              name={item.name}
+              description={item.description}
+              music={item.music}
+              url={item.url}
+            />
+          );
+        })}
       </div>
     </div>
   );
